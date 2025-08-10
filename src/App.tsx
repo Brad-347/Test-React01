c23f2debba5fbfd1e7088dab94f6c5a1da14c531
Brad-347 <bharathi.totakura@gmail.com>
2025-08-10 01:12:31 -0500

    test2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import "./styles.css";
+import VideoListPlayer from "./VideoListPlayer";
 
-export default function App() {
+function App() {
   return (
-    <div style={{ padding: 20, fontFamily: "sans-serif" }}>
-      <h1>OneTube Sample App</h1>
-      <p>Welcome to OneTube MVP!</p>
+    <div>
+      <h1>OneTube Video Player</h1>
+      <VideoListPlayer />
     </div>
   );
 }
+
+export default App;
+
+// export default function App() {
+//   return (
+//     <div style={{ padding: 20, fontFamily: "sans-serif" }}>
+//       <h1>OneTube Sample App</h1>
+//       <p>Welcome to OneTube MVP!</p>
+//       <video width="640" height="360" controls>
+//         <source
+//           src="https://onetube-videos-347.s3.us-east-1.amazonaws.com/Sample01.mp4"
+//           type="video/mp4"
+//         />
+//         Your browser does not support the video tag.
+//       </video>
+//     </div>
+//   );
+// }
---
+++ src/VideoListPlayer.jsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div style={{ padding: 20, fontFamily: "sans-serif" }}>
+      <h1>OneTube Sample App</h1>
+      <p>Welcome to OneTube MVP!</p>
+    </div>
+  );
+}
